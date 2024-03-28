dc347f05eeb08f9574a0f1ea9a98570d4bd91d9a
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-03-28 10:56:27 +0900

    edit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,6 +11,7 @@ public static class MauiProgram
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
             {
+                fonts.AddFont("Meiryo-UI-W53-Regular.ttf", "MeiryoUIRegular");
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
