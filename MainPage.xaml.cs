b8da5d347b390b7fabf9c923fc1faeaef556df2f
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-03-28 18:01:26 +0900

    edit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,32 @@
-﻿namespace MauiSample;
+﻿using System.Collections.ObjectModel;
+
+namespace MauiSample;
+
+public class ProductRecord
+{
+    public ObservableCollection<ProductRecord> ProductRecords { get; private set; } = new ObservableCollection<ProductRecord>();
+
+    public ProductRecord()
+    {
+        ProductRecords.Add(new ProductRecord()
+        {
+            ProductNumber = "189750-000",
+            UniformNumber = "◯✖️△□",
+            AccommodateNumber = 30,
+        });
+
+    }
+    public string ProductNumber { get; set; }
+    public string UniformNumber { get; set; }
+    public int AccommodateNumber { get; set; }
+}
 
 public partial class MainPage : ContentPage
 {
-
     public MainPage()
     {
         InitializeComponent();
+        // BindingContext = new ProductRecord();
     }
 
     async void OnCloseClicked(object sender, EventArgs e)
@@ -18,4 +39,25 @@ public partial class MainPage : ContentPage
             Application.Current.Quit();
         }
     }
+
+    async void OnResetClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("テスト", "[リセット]ボタンクリック", "Yes", "No");
+    }
+
+    async void OnBindClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert("テスト", "[紐づけ処理]ボタンクリック", "Yes", "No");
+    }
+
+    void OnAddProductRecordClicked(object sender, EventArgs e)
+    {
+        // ProductRecords.Add(new ProductRecord()
+        // {
+        //     ProductNumber = "189750-000",
+        //     UniformNumber = "◯✖️△□",
+        //     AccommodateNumber = 30,
+        // });
+    }
+
 }
