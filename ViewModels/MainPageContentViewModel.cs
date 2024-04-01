f9f5606b00f832f0a2836166921d316157aaacdb
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-04-01 15:57:07 +0900

    edit

---
+++ ViewModels/MainPageContentViewModel.cs
@@ -0,0 +1,9 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiSample.ViewModels;
+
+public partial class MainPageContentViewModel : ObservableObject
+{
+}
+
---
+++ Views/Controls/MainPageContent.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiSample.Views.Controls;
+
+
+public partial class MainPageContent : ContentView
+{
+    public MainPageContent()
+    {
+        InitializeComponent();
+    }
+}
