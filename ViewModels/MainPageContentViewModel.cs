23cf132ad54f54b125a81d555493272fdb606c7f
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-04-01 17:22:04 +0900

    edit

--- ViewModels/MainPageContentViewModel.cs
+++ ViewModels/MainPageContentViewModel.cs
@@ -5,5 +5,24 @@ namespace MauiSample.ViewModels;
 
 public partial class MainPageContentViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string okamochiQr; // -> OkamochiQr
+
+    [ObservableProperty]
+    private string kanbanQr; // -> KanbanQr
+
+    public MainPageContentViewModel()
+    {
+        OkamochiQr = "OKA_F_    PD03_017";
+        KanbanQr = "◯✖️△□";
+    }
+
+
+    [RelayCommand]
+    void Reset() // -> ResetCommand
+    {
+        OkamochiQr = string.Empty;
+        KanbanQr = string.Empty;
+    }
 }
 
