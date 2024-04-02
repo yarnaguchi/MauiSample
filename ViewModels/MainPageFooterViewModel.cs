4d72703dd30bbd5b50328a019324b4302b4ec370
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-04-02 14:17:12 +0900

    edit

---
+++ ViewModels/MainPageFooterViewModel.cs
@@ -0,0 +1,12 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace MauiSample.ViewModels;
+
+public partial class MainPageFooterViewModel : ObservableObject
+{
+    public MainPageFooterViewModel()
+    {
+    }
+}
+
