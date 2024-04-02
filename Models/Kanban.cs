83d3bfffdb4d14630e67eebe8c6fa59f7facb1c3
山口絃 <gen.yamaguchi@exc.tokai-rika.co.jp>
2024-04-02 17:11:46 +0900

    Update Kanban.cs

--- Models/Kanban.cs
+++ Models/Kanban.cs
@@ -2,7 +2,12 @@ namespace MauiSample.Models;
 
 public class Kanban
 {
+    // ¶Y•i”Ô_İ’è‚È‚µ
     public string ProductNumber { get; set; }
+
+    // ”w”Ô†
     public string UniformNumber { get; set; }
+
+    // û—e”
     public string AccommodateNumber { get; set; }
 }
